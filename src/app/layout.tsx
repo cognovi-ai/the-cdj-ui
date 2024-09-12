b953329b88656c234bf1d40866a4f9aeb5536f65
Ishaan bhalla <46517523+ishaan000@users.noreply.github.com>
2024-09-12 13:34:03 -0700

    Initial NextJS commit.  (#1)
    * Initial NextJS commit.
    
    Setup NextJS application using TypeScript and App Router.
    Cleaned up boilerplate code.
    Installed material UI.
    Setup strict tsconfig.
    Updated package.json dev script to use port 8080.
    Updated README.
    
    * Delete init svg files.
    
    ---------
    
    Co-authored-by: Ryan James Meneses <github@ryanmeneses.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+
+ export default function RootLayout(props: any) {
+   return (
+     <html lang="en">
+       <body>
+        <AppRouterCacheProvider>
+           {props.children}
+        </AppRouterCacheProvider>
+       </body>
+     </html>
+   );
+ }
