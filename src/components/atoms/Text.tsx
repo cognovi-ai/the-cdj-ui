477d1cf81944ea2226e18620829e2f36577a5241
Ryan James Meneses <github@ryanmeneses.com>
2024-09-16 15:41:14 -0700

    Resolve module not found build error.
    - Updated next.config.mjs to resolve '@components' alias in webpack.
    - Created types directory:
      - Moved TextProps interface to the types directory, declaring it as a module for global access to the TextProps type from the Text component.
      - Imported the type into the Text component for usage.
    
    - Updated tsconfig:
      - Changed target and module to ESNext for better compatibility and faster builds.
      - Removed outDir as build files are placed in the .next directory in Next.js projects.
      - Updated jsx setting to 'preserve', in accordance with automatic fixes from the Next.js compiler.
      - Simplified include paths.

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -1,20 +1,12 @@
-import React from 'react';
-import { Typography } from '@mui/material';
-
-interface TextProps {
-  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body1' | 'body2' | 'button' | 'caption' | 'overline' | 'subtitle1' | 'subtitle2' ;
-  align?: 'inherit' | 'left' | 'center' | 'right' | 'justify';
-  color?: 'initial' | 'inherit' | 'primary' | 'secondary' | 'textPrimary' | 'textSecondary' | 'error';
-  gutterBottom?: boolean;
-  noWrap?: boolean;
-  children: React.ReactNode;
-}
+import React from "react";
+import { Typography } from "@mui/material";
+import { TextProps } from "@components/atoms/Text";
 
 const Text: React.FC<TextProps> = ({
   variant,
-  align = 'inherit',   
-  color = 'textPrimary',
-  gutterBottom = false,     
+  align = "inherit",
+  color = "textPrimary",
+  gutterBottom = false,
   noWrap = false,
   children,
 }: TextProps) => {
