8cf58aba9dc26d2b577479f5a87980be4d9e19af
Ryan James Meneses <github@ryanmeneses.com>
2024-09-19 17:02:13 -0700

    fix: Resolve ReferenceError: React is not defined.
    Imports React into layout.tsx and page.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
- export default function RootLayout(props: any) {
-   return (
-     <html lang="en">
-       <body>
-        <AppRouterCacheProvider>
-           {props.children}
-        </AppRouterCacheProvider>
-       </body>
-     </html>
-   );
- }
+export default function RootLayout(props: any) {
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider>{props.children}</AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
