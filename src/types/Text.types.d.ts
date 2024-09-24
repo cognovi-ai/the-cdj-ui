35bb193120885686d002812ac4b5d123ff15f23f
Ryan James Meneses <58452495+hiyaryan@users.noreply.github.com>
2024-09-24 15:47:13 -0700

    style: update eslint (#10)
    - Extends ESLint rules.
    - Integrates Prettier with ESLint.
    - Fixes style errors.

--- src/types/Text.types.d.ts
+++ src/types/Text.types.d.ts
@@ -1,15 +1,35 @@
 declare module '@components/atoms/Text' {
-    import { ReactNode, FC } from 'react';
-  
-    export interface TextProps {
-      variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body1' | 'body2' | 'button' | 'caption' | 'overline' | 'subtitle1' | 'subtitle2';
-      align?: 'inherit' | 'left' | 'center' | 'right' | 'justify';
-      color?: 'initial' | 'inherit' | 'primary' | 'secondary' | 'textPrimary' | 'textSecondary' | 'error';
-      gutterBottom?: boolean;
-      noWrap?: boolean;
-      children: ReactNode;
-    }
-  
-    const Text: FC<TextProps>;
-    export default Text;
+  import { FC, ReactNode } from 'react';
+
+  export interface TextProps {
+    variant?:
+      | 'h1'
+      | 'h2'
+      | 'h3'
+      | 'h4'
+      | 'h5'
+      | 'h6'
+      | 'body1'
+      | 'body2'
+      | 'button'
+      | 'caption'
+      | 'overline'
+      | 'subtitle1'
+      | 'subtitle2';
+    align?: 'inherit' | 'left' | 'center' | 'right' | 'justify';
+    color?:
+      | 'initial'
+      | 'inherit'
+      | 'primary'
+      | 'secondary'
+      | 'textPrimary'
+      | 'textSecondary'
+      | 'error';
+    gutterBottom?: boolean;
+    noWrap?: boolean;
+    children: ReactNode;
+  }
+
+  const Text: FC<TextProps>;
+  export default Text;
 }
