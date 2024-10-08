a605ffddd3b7b7ec7e9fe7a3d0ebaadb860fe3fd
Ishaan bhalla <46517523+ishaan000@users.noreply.github.com>
2024-10-08 16:11:05 -0700

    feat!: add input atomic component (#9)
    * feat!: add input atomic component
    
    - Added Input.tsx and Input.types.d.ts.
      - Wrapped MuiInput in new Input component.
    - Added test suite for input, Input.test.tsx.
    
    * feat: add accessibility props
    
    - Added aria-label, tabIndex and ariaLabelledBy to Input.tsx and Input.types.d.ts.
    - Added accessibility tests in input test suite.
    
    * style: fix linter errors

---
+++ src/types/Input.types.d.ts
@@ -0,0 +1,26 @@
+declare module '@components/atoms/Input' {
+  import { ChangeEventHandler, FC } from 'react';
+  import { TextFieldProps as MuiInputProps } from '@mui/material/TextField';
+
+  export interface InputProps
+    extends Omit<MuiInputProps, 'variant' | 'onChange'> {
+    label: string;
+    type?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+    value?: string | number;
+    variant?: 'filled' | 'outlined' | 'standard';
+    placeholder?: string;
+    helperText?: string;
+    error?: boolean;
+    disabled?: boolean;
+    multiline?: boolean;
+    rows?: number;
+    sx?: object;
+    tabIndex?: number;
+    ariaLabel?: string;
+    ariaLabelledBy?: string;
+  }
+
+  const Input: FC<InputProps>;
+  export default Input;
+}
