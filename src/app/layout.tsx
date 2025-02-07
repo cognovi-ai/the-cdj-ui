000025e63d538c75ffbb8e6c98d057a850e2176f
ishaan <ishaanbhalla1998@gmail.com>
2025-02-07 10:43:14 -0800

    refactor(layout): integrate FlashContext into layout - Wrap application with FlashContext provider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { CssBaseline } from '@mui/material';
+import { FlashComponent } from '../components/utils/FlashComponent';
+import { FlashProvider } from '../contexts/FlashProvider';
 import { JournalProvider } from '../contexts/JournalProvider';
 import React from 'react';
 import { ThemeProvider } from '../theme/ThemeContext';
@@ -18,9 +20,12 @@ export default function RootLayout({
       <body>
         <ThemeProvider>
           <CssBaseline />
-          <JournalProvider>
-            <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-          </JournalProvider>
+          <FlashProvider>
+            <FlashComponent /> 
+            <JournalProvider>
+              <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+            </JournalProvider>
+          </FlashProvider>
         </ThemeProvider>
       </body>
     </html>
